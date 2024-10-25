15128141e17fcc24f1d1f34dbb27b5a4a90c550b
NagarajuThulluru <thullurunagaraju9@gmail.com>
2024-10-25 16:09:07 +0530

    first commit

---
+++ src/test/java/verifications/OpenSpan_Login.java
@@ -0,0 +1,42 @@
+package verifications;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OpenSpan_Login {
+	static WebDriver driver;
+	
+	public static void InvokeBrowser() {
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+		
+	}
+	public static void Login() {
+		driver.get("https://training.openspan.com/login");
+		driver.findElement(By.id("user_name")).sendKeys("kaiu");
+		driver.findElement(By.id("user_pass")).sendKeys("JASUYG");
+		driver.findElement(By.id("login_button")).click();
+		driver.close();
+	}
+	
+	
+
+	public static void main(String[] args) {
+		
+		OpenSpan_Login.InvokeBrowser();
+		OpenSpan_Login.Login();
+
+		
+	}
+	
+
+}
